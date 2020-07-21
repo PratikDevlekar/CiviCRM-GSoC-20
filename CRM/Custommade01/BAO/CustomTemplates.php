fbf698fef5eeead3633297ae6d501a175dd386bb
Pratik Devlekar <pratikcodehunt@gmail.com>
2020-07-21 22:58:22 +0530

    Added rest api

--- CRM/Custommade01/BAO/CustomTemplates.php
+++ CRM/Custommade01/BAO/CustomTemplates.php
@@ -22,13 +22,32 @@ class CRM_Custommade01_BAO_CustomTemplates extends CRM_Custommade01_DAO_CustomTe
 
     return $instance;
   }
-  
+  // Delete the template
   public static function del($id) {
     $instance = new CRM_Custommade01_DAO_CustomTemplates();
     $instance->id = $id;
     $instance->delete();
   }
+  // Call rest api 
+  public static function mjmlToHtml($CustomFt) {
+    $url = 'https://api.mjml.io/v1/render';
 
+    $username = 'd2adb815-1d2f-4135-9ded-90883b620335';
+    $password = '57cc8c48-abed-4158-89be-b84dad5aa7f9';
+    $curl = curl_init();
 
+    curl_setopt($curl, CURLOPT_POST, 1);
+    $CustomFt = is_array($CustomFt) ? json_encode($customFt) : $CustomFt;
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $CustomFt);
+
+    curl_setopt($curl, CURLOPT_USERPWD, "{$username}:{$password}");
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    $result = curl_exec($curl);
+    curl_close($curl);
+
+    return json_decode($result, TRUE);
+  }
 
 }
